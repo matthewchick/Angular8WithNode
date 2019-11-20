de52caff77d558c1c965b525889c7fa102014090
Matthew Chick <c.chick@unsw.edu.au>
2019-11-20 21:01:20 +1100

    implement two ways binding

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,7 @@
+
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
@@ -13,6 +15,7 @@ import { MatInputModule, MatCardModule, MatButtonModule } from '@angular/materia
   ],
   imports: [
     BrowserModule,
+    FormsModule,
     BrowserAnimationsModule,
     MatInputModule,
     MatCardModule,
--- src/app/posts/post-create/post-create.component.ts
+++ src/app/posts/post-create/post-create.component.ts
@@ -6,7 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./post-create.component.css']
 })
 export class PostCreateComponent implements OnInit {
-
+  enteredValue = '';
   newPost = 'NO CONTENT';
   onAddPost(postInput: HTMLTextAreaElement) {
     console.log(postInput);
